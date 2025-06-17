37820c4e205b467db2ad91f963843da8d71c777f
flopes679640 <135030906+flopes679640@users.noreply.github.com>
2025-06-17 19:29:26 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ let mensagem = document.querySelector('#mensagem');
 const botao = document.querySelector('.botaoMensagem');
 
 botao.addEventListener('click', e=>{
-let conteudoMensagem = "Continue se dedicando, que voce alcançara todos os seus objetivos";
+let conteudoMensagem = "Continue se dedicando, que você alcançara todos os seus objetivos";
  mensagem.innerHTML = conteudoMensagem;
 setTimeout(() => {
  mensagem.innerHTML = '';
